78a28f5804f74a9866d29a4cafc763a2a044b8d2
Romman Sabbir <misterxero96@gmail.com>
2019-02-24 11:41:21 +0600

    added: ParseLiveQuery-Android library changes: Parse live query callback listener moved to library

--- app/src/main/java/com/rommansabbir/back4app/livequeryexample/MainActivity.java
+++ app/src/main/java/com/rommansabbir/back4app/livequeryexample/MainActivity.java
@@ -3,35 +3,57 @@ package com.rommansabbir.back4app.livequeryexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
-import com.rommansabbir.back4app.livequeryexample.callbacks.ParseLiveQueryCallback;
+import com.parse.ParseQuery;
 import com.parse.ParseObject;
+import com.rommansabbir.parselivequery.ParseLiveQueryListener;
 
-public class MainActivity extends AppCompatActivity implements ParseLiveQueryCallback.ParseLiveQueryInterface {
+public class MainActivity extends AppCompatActivity implements ParseLiveQueryListener.ParseLiveQueryListenerInterface {
     private static final String TAG = "MainActivity";
+    private ParseLiveQueryListener parseLiveQueryListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //instantiate ParseLiveQueryCallback
-        ParseLiveQueryCallback parseLiveQueryCallback = new ParseLiveQueryCallback(this);
+        /**
+         * Instantiate ParseLiveQueryListener
+         */
+        parseLiveQueryListener = new ParseLiveQueryListener(this);
 
-        //call getLiveQueryData() from callback to activate liveQuery
-        parseLiveQueryCallback.getLiveQueryData("swurvintest", "Accident");
+        /**
+         * Create a usual parse query, add some constraint if you want
+         */
+        ParseQuery<ParseObject> liveQuery = ParseQuery.getQuery("CLASS_NAME_HERE");
+
+        /**
+         * Add live event listener to your query
+         * Provide the name of your app, pass the liveQuery to the live event listener
+         * @APP_NAME
+         * @liveQuery
+         */
+        parseLiveQueryListener.addLiveEventListener("APP_NAME", liveQuery);
 
     }
 
     @Override
-    public void onSuccess(ParseObject retrievedObject) {
+    public void onEventListenerSuccess(ParseObject retrievedObject) {
         //TODO implement your logic here
-        Log.d(TAG, "onSuccess: ");
+        Log.d(TAG, "onEventListenerSuccess: ");
 
     }
 
     @Override
-    public void onFailure(String errorMessage) {
+    public void onEventListenerFailure(String errorMessage) {
         //TODO implement your logic here
-        Log.d(TAG, "onFailure: ");
+        Log.d(TAG, "onEventListenerFailure: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        /**
+         * Destroy the callback after it usages
+         */
+        parseLiveQueryListener.destroyCallback();
     }
 }
